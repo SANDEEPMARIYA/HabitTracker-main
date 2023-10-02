f0e72927fdfd471401c90bcfd7403f2273e5d071
SANDEEPMARIYA <136779150+SANDEEPMARIYA@users.noreply.github.com>
2023-10-03 02:19:47 +0530

    Update db.js

--- app/db.js
+++ app/db.js
@@ -1,11 +1,26 @@
-const dotenv = require("dotenv");
-dotenv.config();
 
-const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://sandswet05:Sand@2002@cluster0.gwxxoel.mongodb.net/?retryWrites=true&w=majority";
 
-exports.connectMongoose = () => {
-    mongoose.set("strictQuery", false);
-    mongoose.connect('mongodb+srv://sandswet05:Sand%402002@cluster0.gwxxoel.mongodb.net/csv-upload?retryWrites=true&w=majority', { useNewUrlParser: true })
-        .then(() => console.log("Connected to Mongodb => Habit-Tracker"))
-        .catch((e) => console.log("Not Connected to Mongodb", e));
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
+run().catch(console.dir);
