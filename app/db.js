d6063e935421b584e12c410ec46563d97c5d697b
SANDEEPMARIYA <136779150+SANDEEPMARIYA@users.noreply.github.com>
2023-10-03 02:15:46 +0530

    Update db.js

--- app/db.js
+++ app/db.js
@@ -1,12 +1,11 @@
 const dotenv = require("dotenv");
-
 dotenv.config();
 
 const mongoose = require('mongoose');
 
-exports.connectMongoose =()=>{
+exports.connectMongoose = () => {
     mongoose.set("strictQuery", false);
-    mongoose.connect('mongodb+srv://sandswet05:Sand@2002@cluster0.gwxxoel.mongodb.net/csv-upload?retryWrites=true&w=majority', { useNewUrlParser: true })
-    .then((e)=>console.log("Connected to Mongodb => Habit-Tracker"))
-    .catch((e)=>console.log("Not Connect Mongodb", e));
+    mongoose.connect('mongodb+srv://sandswet05:Sand%402002@cluster0.gwxxoel.mongodb.net/csv-upload?retryWrites=true&w=majority', { useNewUrlParser: true })
+        .then(() => console.log("Connected to Mongodb => Habit-Tracker"))
+        .catch((e) => console.log("Not Connected to Mongodb", e));
 }
