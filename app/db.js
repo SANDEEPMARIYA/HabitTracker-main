0ff8d7b16669d44037d177a52356dda7ec05c629
SANDEEPMARIYA <136779150+SANDEEPMARIYA@users.noreply.github.com>
2023-10-03 01:36:30 +0530

    Update db.js

--- app/db.js
+++ app/db.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 exports.connectMongoose =()=>{
     mongoose.set("strictQuery", false);
     mongoose.connect('mongodb+srv://sandswet05:Sand@2002@cluster0.gwxxoel.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
-    .then((e)=>console.log("Connected to Mongodb"))
+    .then((e)=>console.log("Connected to Mongodb => Habit-Tracker
+.Habit-Tracker"))
     .catch((e)=>console.log("Not Connect Mongodb", e));
 }
